1857017cb648d7768765af7d5a481a909b1dd21d
Oliwia Brajewska <brajewska.oliwia8@gmail.com>
2021-12-13 22:51:15 +0100

    logowanko

---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <!-- Be sure to leave the brand out there if you want it shown -->
+      <a class="brand" href="#">Zero Waste</a>
+ 
+      <!-- Everything you want hidden at 940px or less, place within here -->
+      <div class="nav-collapse collapse">
+        <!-- .nav, .navbar-search, .navbar-form, etc -->
+        <form class="navbar-search pull-rigth">
+  <input type="text" class="search-query" placeholder="Wyszukaj co cię interesuję">
+</form>
+
+      </div>
+ 
+    </div>
+  </div>
+</div>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+@section("content")
+<h1>Login page</h1>
+<button>Zaloguj się</button>
+@endsection
