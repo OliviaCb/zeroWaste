7bd1bb692b2b5fbe13975eac6db0794bd3c46777
Rax232 <mrrax333@gmail.com>
2021-12-28 20:42:20 +0100

    eee
    asd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\displayController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,26 +35,6 @@ Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showRese
 Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 
 Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\displayController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/homePage', function () {
     return view('homePage', ['name' => 'Jan', 'surname' => 'Kowalski']);
