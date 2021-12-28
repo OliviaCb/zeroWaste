3d4b31a7e217852e86a9b41facd4a6121755b955
Rax232 <mrrax333@gmail.com>
2021-12-28 20:41:06 +0100

    poprawka web.php
    poprawka web.php

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,35 @@ Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showRese
 Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 
 Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\displayController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/homePage', function () {
+    return view('homePage', ['name' => 'Jan', 'surname' => 'Kowalski']);
+});
+
+Route::get('db', [displayController::class, 'db']);
+
+//Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('recipe', 'RecipesController');
