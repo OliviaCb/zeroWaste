c3afef29697025fef6759b8630474984026623c1
BukowskiPiotr <ilovemusic640@gmail.com>
2021-12-29 13:39:29 +0100

    poprawilem env
    xd

---
+++ example/storage/framework/views/0ed3c027c18eeedbc4d12e79d988bc0f6c43485a.php
@@ -0,0 +1,17 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+<h1>Przepisy</h1>
+<?php if(count($recipes)>1): ?>
+<?php $__currentLoopData = $recipes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $recipe): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<div class="well">
+    <h3><?php echo e($recipe->title); ?></h3>
+</div>
+
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php else: ?>
+
+<?php endif; ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\bukow\Documents\GitHub\zeroWaste\example\resources\views/recipes/recipes.blade.php ENDPATH**/ ?>
