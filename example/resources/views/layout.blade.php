61f8c0e243b9f9a8947253033b17968968e7013d
Rax232 <mrrax333@gmail.com>
2021-12-29 21:11:03 +0100

    Update

--- example/resources/views/layout.blade.php
+++ example/resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 
 <head>
 
-    <title>Laravel - ItSolutionStuff.com</title>
+    <title>Zerowaste</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
