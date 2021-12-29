3757f868875a628160902145624e18d9c3c5d617
BukowskiPiotr <ilovemusic640@gmail.com>
2021-12-29 15:24:12 +0100

    wyswietlanie przepisow tabelarycznie

--- example/resources/views/recipes/recipes.blade.php
+++ example/resources/views/recipes/recipes.blade.php
@@ -3,6 +3,40 @@
 @section('content')
 
 <h1>Przepisy</h1>
+
+<!--tutaj powstaje wyswietlanie przepisów-->
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Tytuł</th>
+      <th scope="col">Zdjęcie</th>
+      <th scope="col">Składniki</th>
+      <th scope="col">Urządzenia kuchenne</th>
+      <th scope="col">Opis przygotowania</th>
+      <th scope="col">Czas przygotowania</th>
+      <th scope="col">Trudność przygotowania</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($recipes as $recipe)
+    <tr>
+      <th scope="row">{{$recipe->recipe_id}}</th>
+      <td>{{$recipe->title}}</td>
+      <td>{{$recipe->photo}}</td>
+      <td>{{$recipe->products}}</td>
+      <td>{{$recipe->food_processors}}</td>
+      <td>{{$recipe->description}}</td>
+      <td>{{$recipe->time}} min</td>
+      <td>{{$recipe->level}}/5</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
+<!--zakończenie wyświetlania przepisów-->
+
 @if(count($recipes)>1)
 @foreach($recipes as $recipe)
 <div class="well">
--- example/storage/framework/views/0ed3c027c18eeedbc4d12e79d988bc0f6c43485a.php
+++ example/storage/framework/views/0ed3c027c18eeedbc4d12e79d988bc0f6c43485a.php
@@ -3,6 +3,40 @@
 <?php $__env->startSection('content'); ?>
 
 <h1>Przepisy</h1>
+
+<!--tutaj powstaje wyswietlanie przepisów-->
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Tytuł</th>
+      <th scope="col">Zdjęcie</th>
+      <th scope="col">Składniki</th>
+      <th scope="col">Urządzenia kuchenne</th>
+      <th scope="col">Opis przygotowania</th>
+      <th scope="col">Czas przygotowania</th>
+      <th scope="col">Trudność przygotowania</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php $__currentLoopData = $recipes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $recipe): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <tr>
+      <th scope="row"><?php echo e($recipe->recipe_id); ?></th>
+      <td><?php echo e($recipe->title); ?></td>
+      <td><?php echo e($recipe->photo); ?></td>
+      <td><?php echo e($recipe->products); ?></td>
+      <td><?php echo e($recipe->food_processors); ?></td>
+      <td><?php echo e($recipe->description); ?></td>
+      <td><?php echo e($recipe->time); ?> min</td>
+      <td><?php echo e($recipe->level); ?>/5</td>
+    </tr>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+  </tbody>
+</table>
+
+<!--zakończenie wyświetlania przepisów-->
+
 <?php if(count($recipes)>1): ?>
 <?php $__currentLoopData = $recipes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $recipe): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 <div class="well">
@@ -14,4 +48,5 @@
 
 <?php endif; ?>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\bukow\Documents\GitHub\zeroWaste\example\resources\views/recipes/recipes.blade.php ENDPATH**/ ?>
