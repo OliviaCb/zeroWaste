f2ccf8cdffdf6d72428466add8d5e013f2d8b42a
BukowskiPiotr <ilovemusic640@gmail.com>
2022-01-01 17:40:26 +0100

    diplay przepisów git

--- example/storage/framework/views/58193a059fcf09651de7a24dd30decbcea653398.php
+++ example/storage/framework/views/58193a059fcf09651de7a24dd30decbcea653398.php
@@ -65,21 +65,27 @@
     <dl class="row">
       <dt class="col-sm-3">Nazwa potrawy</dt>
       <dd class="col-sm-9"><?php echo e($recipe->title); ?></dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3">Zdjęcie</dt>
       <dd class="col-sm-9"><img src="<?php echo e(asset('../public/uploads/recipes/'.$recipe->photo)); ?>" width="70px" height = "70px" alt=""></dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3">Składniki</dt>
       <dd class="col-sm-9"><?php echo e($recipe->products); ?></dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3 text-truncate">Maszyny kuchenne</dt>
       <dd class="col-sm-9"><?php echo e($recipe->food_processors); ?></dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3 text-truncate">Czas przygotowania</dt>
       <dd class="col-sm-9"><?php echo e($recipe->time); ?> min</dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3 text-truncate">Trudność przygotowania</dt>
       <dd class="col-sm-9"><?php echo e($recipe->level); ?>/5</dd>
+      <HR NOSHADE>
 
       <dt class="col-sm-3 text-truncate">Opisz przygotowania</dt>
       <dd class="col-sm-9"><?php echo e($recipe->description); ?></dd>
